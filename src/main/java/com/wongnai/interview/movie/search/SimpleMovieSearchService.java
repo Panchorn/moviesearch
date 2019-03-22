e734a600e9bc4e6476218cc4905c3719c21c895a
Panchorn Lertvipada <nonpcn@gmail.com>
2019-03-22 23:56:12 +0700

    Implement feature search by using Matcher

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,24 +1,42 @@
 package com.wongnai.interview.movie.search;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.wongnai.interview.movie.Movie;
 import com.wongnai.interview.movie.MovieSearchService;
 import com.wongnai.interview.movie.external.MovieDataService;
+import com.wongnai.interview.movie.external.MoviesResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Component("simpleMovieSearchService")
 public class SimpleMovieSearchService implements MovieSearchService {
-	@Autowired
-	private MovieDataService movieDataService;
+    @Autowired
+    private MovieDataService movieDataService;
 
-	@Override
-	public List<Movie> search(String queryText) {
-		//TODO: Step 2 => Implement this method by using data from MovieDataService
-		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
-		// Please do not change @Component annotation on this class
-		return null;
-	}
+    @Override
+    public List<Movie> search(String queryText) {
+        //TODO: Step 2 => Implement this method by using data from MovieDataService
+        // All test in SimpleMovieSearchServiceIntegrationTest must pass.
+        // Please do not change @Component annotation on this class
+
+        MoviesResponse moviesResponse = movieDataService.fetchAll();
+
+        return moviesResponse
+                .stream()
+                .filter(m -> {
+                    Pattern pattern = Pattern.compile("\\b(" + queryText.toLowerCase() + ")\\b");
+                    Matcher matcher = pattern.matcher(m.getTitle().toLowerCase());
+                    return matcher.find();
+                })
+                .map(movieData -> {
+                    Movie m = new Movie(movieData.getTitle());
+                    m.setActors(movieData.getCast());
+                    return m;
+                })
+                .collect(Collectors.toList());
+    }
 }
