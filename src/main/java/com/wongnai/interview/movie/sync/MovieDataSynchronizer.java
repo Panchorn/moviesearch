e2eb8ada0ba73dcf16f2def60f4cee1be9b6d97c
Panchorn Lertvipada <nonpcn@gmail.com>
2019-03-23 01:24:14 +0700

    Implement feature forceSync for get and save data to database

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,6 +2,8 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +12,18 @@ import com.wongnai.interview.movie.external.MovieDataService;
 
 @Component
 public class MovieDataSynchronizer {
-	@Autowired
-	private MovieDataService movieDataService;
+    @Autowired
+    private MovieDataService movieDataService;
 
-	@Autowired
-	private MovieRepository movieRepository;
+    @Autowired
+    private MovieRepository movieRepository;
 
-	@Transactional
-	public void forceSync() {
-		//TODO: implement this to sync movie into repository
-	}
+    @Transactional
+    public void forceSync() {
+        //TODO: implement this to sync movie into repository
+        MoviesResponse moviesResponse = movieDataService.fetchAll();
+        moviesResponse
+                .stream()
+                .forEach(m -> movieRepository.save(new Movie(m.getTitle(), m.getCast())));
+    }
 }
