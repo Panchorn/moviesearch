68472a3a38f7c6869d8541e44f049caf280d723f
Panchorn Lertvipada <nonpcn@gmail.com>
2019-03-23 19:11:06 +0700

    Create map and put movie index to it

---
+++ src/main/java/com/wongnai/interview/MovieConstant.java
@@ -0,0 +1,14 @@
+package com.wongnai.interview;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class MovieConstant {
+
+    public static Map<String, List<Long>> MAP_INDEX = new HashMap<>();
+
+}
--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,6 +2,7 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.MovieConstant;
 import com.wongnai.interview.movie.Movie;
 import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,11 @@ import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
@@ -22,8 +28,28 @@ public class MovieDataSynchronizer {
     public void forceSync() {
         //TODO: implement this to sync movie into repository
         MoviesResponse moviesResponse = movieDataService.fetchAll();
-        moviesResponse
+        List<Movie> movieList = moviesResponse
                 .stream()
-                .forEach(m -> movieRepository.save(new Movie(m.getTitle(), m.getCast())));
+                .map(m -> movieRepository.save(new Movie(m.getTitle(), m.getCast())))
+                .collect(Collectors.toList());
+        initialMapIndex(movieList);
+    }
+
+    private void initialMapIndex(List<Movie> movieList) {
+        movieList
+                .stream()
+                .forEach(m -> {
+                    List<String> key = Arrays.asList(m.getName().split(" "));
+                    key.stream().forEach(s -> putIndexToMap(s.toLowerCase(), m.getId()));
+                });
+    }
+
+    private void putIndexToMap(String key, Long movieId) {
+        List<Long> movieIdList = new LinkedList<>();
+        if (MovieConstant.MAP_INDEX.containsKey(key)) {
+            movieIdList = MovieConstant.MAP_INDEX.get(key);
+        }
+        movieIdList.add(movieId);
+        MovieConstant.MAP_INDEX.put(key, movieIdList);
     }
 }
